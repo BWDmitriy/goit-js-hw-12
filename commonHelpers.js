cd5b5fe7b14b1203bb2dcae41fe82d4c954c6a76
BWDmitriy <82403947+BWDmitriy@users.noreply.github.com>
2024-03-03 11:10:52 +0000

    Deploying to gh-pages from @ BWDmitriy/goit-js-hw-12@59d1caea0be638ecff53c5e1a5492a460a9628a8 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,23 +1,2 @@
-import{i as u,S as d}from"./assets/vendor-5b791d57.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const l of e.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function n(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function s(t){if(t.ep)return;t.ep=!0;const e=n(t);fetch(t.href,e)}})();function h(r){return r=encodeURIComponent(r),i.innerHTML='<div class="loader"></div>',fetch(`https://pixabay.com/api/?key=42609290-856768105ab9e79485c69bf61&q=${r}&image_type=photo&orientation=horizontal&safesearch=true`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()})}function p(r){i.innerHTML="",console.log(r);const o=r.hits;o.length==0&&u.error({title:"Error",message:"Error: No such pictures!",position:"topRight"});const n=o.map(e=>`<li class="gallery-item">
-        <a class="gallery-link" href="${e.largeImageURL}">
-          <img
-            class="gallery-image"
-            width="1280"
-            height="152"
-            src="${e.webformatURL}"
-            data-source="${e.largeImageURL}"
-            alt="${e.tags}"
-          />
-          <ul class="gallery-description">
-          <li><h3>Likes</h3><p>${e.likes}</p>
-          </li>
-          <li><h3>Views</h3><p>${e.views}</p>
-            </li>
-            <li><h3>Comments</h3><p>${e.comments}</p>
-              </li>
-              <li><h3>Downloads</h3><p>${e.downloads}</p>
-                </li>
-          </ul>
-        </a>
-      </li>`).join("");i.insertAdjacentHTML("beforeend",n);const s={captionsData:"alt"};let t=new d(".gallery a",s);t.on("show.simplelightbox",function(){}),t.refresh()}const i=document.querySelector("ul.gallery");let c="";const a=document.getElementById("search-input");a.addEventListener("input",r=>{c=a.value.trim(),i.innerHTML=""});const f=document.getElementById("search-button");f.addEventListener("click",()=>{c&&(i.innerHTML='<div class="loader"></div>',h(c).then(r=>p(r)).catch(r=>{console.log(r),u.error({title:"Error",message:"Виникла помилка під час завантаження зображень. Будь ласка, спробуйте пізніше.",position:"topRight"})}))});
+import{a as y,i as u}from"./assets/vendor-22aaac4a.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();async function d(r){r=encodeURIComponent(r),l.innerHTML='<div class="loader"></div>';const o="42609290-856768105ab9e79485c69bf61",n=new URLSearchParams({_limit:m,_page:s,_key:o,_image_type:"photo",_orientation:"horizontal",_safesearch:"true"});return(await y.get(`https://pixabay.com/api/posts?${n}&q=${r}`)).data}const l=document.querySelector("ul.gallery");let c="";const p=document.getElementById("search-input");let s=1,m=15;const g=Math.ceil(100/m);p.addEventListener("input",r=>{c=p.value.trim(),l.innerHTML=""});const f=document.getElementById("search-button");f.addEventListener("click",async()=>{if(l.innerHTML='<div class="loader"></div>',s>g)return u.error({position:"topRight",message:"We're sorry, there are no more posts to load"});try{if(c){const r=await d(c);d(r),s+=1,s>1&&(f.textContent="Fetch more posts")}}catch(r){console.log(r),u.error({title:"Error",message:"Виникла помилка під час завантаження зображень. Будь ласка, спробуйте пізніше.",position:"topRight"})}});
 //# sourceMappingURL=commonHelpers.js.map
